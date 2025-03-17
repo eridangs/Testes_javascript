507e390c523e58682abba7b2162891b28014a82c
Eridan <genealerooo@gmail.com>
2025-03-17 17:00:19 -0400

    Aula comandos de validação
    funcoes como on click, innertext, innerhtml, getelementsbytag e byid

--- index.js
+++ index.js
@@ -1,19 +1,99 @@
-window.alert('Olá, mundo!');
-let nome = String(window.prompt('Qual seu nome?'));
-let segundo_nome = String('Eridan');
-console.log(nome,segundo_nome);
-// document.write('Meu numero ' + 10);
-let text = "Apple, Banana, Kiwi";
-let tamanho = text.length;
-let part = text.slice(7,13);
-console.log(part);
-let text1= "Olá mundo";
-let text2 = text1.toUpperCase();
-idade = parseInt(prompt('Qual sua idade?'));
-// idade=parseInt(idade);
-console.log(typeof idade);
-console.log(`${nome} tem ${idade} anos`);
-function myFunction(p1,p2){
-    return p1 * p2;
+// window.alert('Olá, mundo!');
+// let nome = String(window.prompt('Qual seu nome?'));
+// let segundo_nome = String('Eridan');
+// console.log(nome,segundo_nome);
+// // document.write('Meu numero ' + 10);
+// let text = "Apple, Banana, Kiwi";
+// let tamanho = text.length;
+// let part = text.slice(7,13);
+// console.log(part);
+// let text1= "Olá mundo";
+// let text2 = text1.toUpperCase();
+// idade = parseInt(prompt('Qual sua idade?'));
+// // idade=parseInt(idade);
+// console.log(typeof idade);
+// console.log(`${nome} tem ${idade} anos`);
+// function myFunction(p1,p2){
+//     return p1 * p2;
+// }
+// console.log(myFunction(10,20));
+
+// let idade = 16;
+// if(idade>=16){
+//     console.log('Adolescente');
+// } else if(idade>16 && idade <42) {
+//     console.log('Adulto');
+// }
+// else{
+//     console.log("Idoso");
+// }
+
+// let menu = 1;
+// switch(menu){
+//     case 0:
+//         window.alert('Sair')
+//         break;
+//     case 1:
+//         window.alert('Saldo')
+//         break;
+//         default:
+//         window.alert('Valor Inválido')
+// }
+
+// DATA DE HOJE
+// let menu = 3;
+// switch(new Date().getDay()){
+//     case 0:
+//         day='Domigo';
+//         break;
+//     case 1:
+//         day='Segunda';
+//         break;
+//     case 2:
+//         day='Terça';
+//         break;
+//     case 3:
+//     day='Quarta';
+//         break;
+//     case 4:
+//         day='Quinta';
+//         break;
+//     case 5:
+//         day='Sexta';
+//         break;
+//     case 6:
+//         day='Sabado';
+//         break;   
+// }
+// window.alert(day)
+
+
+// var i = 0
+// while(i<10){
+//     var text;
+//     text = "O número é"+i;
+//     console.log(text);
+//     i++;
+// }
+// // let i = 10
+// do{
+//     text = 'The number is' +i;
+//     i++;
+//     console.log(text);
+// }
+// while(i<10);
+// for(var i = 0; i<9; i++){
+//     console.log(i);
+// }
+
+window.onload=function(){
+    var p1 = document.getElementsByTagName('p')[1];
+    p1.style.background="blue";
 }
-console.log(myFunction(10,20));
+
+// var a = window.document.getElementById("id");
+// a.addEventListener('click',x);
+// function x(){
+//     a.innerText='Clicou';
+//     window.alert('Certa Resposta');
+// }
